817e6c7c35a07a2311ea451a695efdc4686fc876
sleeplesseditor <josh@bryson.me.uk>
2023-12-19 22:59:28 +0000

    Project base files added

---
+++ src/species/Species.jsx
@@ -0,0 +1,12 @@
+export function Species({ name, language, averageLifespan }) {
+    return (
+      <li>
+        {name}
+        <ul>
+          <li>language: {language}</li>
+          <li>average lifespan: {averageLifespan}</li>
+        </ul>
+      </li>
+    );
+  }
+  
