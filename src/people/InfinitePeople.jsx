5f8c853fb4b218ad49a2926dd22439770474e639
Josh Bryson <josh@bryson.me.uk>
2023-12-22 00:22:22 +0000

    Infinite scroll setup added to People and Species variant components

--- src/people/InfinitePeople.jsx
+++ src/people/InfinitePeople.jsx
@@ -1,5 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
+import { useInfiniteQuery } from "react-query"
 
 const initialUrl = "https://swapi.dev/api/people/";
 const fetchUrl = async (url) => {
@@ -8,6 +9,34 @@ const fetchUrl = async (url) => {
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { data, error, fetchNextPage, hasNextPage, isError, isFetching, isLoading } = useInfiniteQuery(
+    "sw-people",
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined
+    }
+  );
+
+  if(isLoading) return <div className="loading">Loading...</div>
+
+  if(isError) return <div>Error! {error.toString()}</div>
+
+  return (
+    <>
+      {isFetching && <div className="loading">Loading...</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map(pageData => {
+          return pageData.results.map((person) => {
+            return (
+              <Person 
+                eyeColor={person.eye_color} 
+                key={person.id} 
+                name={person.name} 
+                hairColor={person.hair_color} 
+              />
+            )})
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
